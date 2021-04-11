4a114f2207505965b5f6754c71996f23819086ed
Nwokoye Chigozie <gregoflash05@users.noreply.github.com>
2021-04-11 14:53:54 +0100

    Test

--- kwek/urls.py
+++ kwek/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
-from graphql_playground.views import GraphQLPlaygroundView
+# from graphql_playground.views import GraphQLPlaygroundView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
