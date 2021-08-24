999aff226683755d062601cd73cfb0458e4c87c7
Nwokoye Chigozie <gregoflash05@users.noreply.github.com>
2021-08-24 23:17:36 +0100

    update

--- kwek/settings.py
+++ kwek/settings.py
@@ -228,9 +228,9 @@ EMAIL_HOST_USER = 'gregoflash01@gmail.com' # this is exactly the value 'apikey'
 EMAIL_HOST_PASSWORD = 'greg1998'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','143.198.115.156', 'kwekapi.com', 'www.kwekapi.com']
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost','143.198.115.156', 'kwekapi.com', 'www.kwekapi.com']
 # CORS_ORIGIN_ALLOW_ALL = False
-# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ORIGIN_WHITELIST = (
