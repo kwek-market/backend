4f06cf2723832335b07cd641f0f47da417b5b945
gregoflash05 <gregoflash05@gmail.com>
2022-02-27 20:51:19 +0000

    made settings generic

--- kwek/heroku_settings.py
+++ kwek/heroku_settings.py
@@ -1,19 +1,29 @@
 
+
 import os
-import django_heroku
 from .general_settings import *
 
-STATIC_URL = "/static/"
-MEDIA_URL = "/asset/"
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATICFILES_DIRS = [BASE_DIR, "static"]
+# STATIC_URL = "/static/"
+# MEDIA_URL = "/asset/"
+
+# STATICFILES_DIRS = [BASE_DIR, "static"]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-STATIC_ROOT = "/home/kwekxbyu/kwekapi.com/static/"
-if DEBUG:
-    MEDIA_ROOT = os.path.join(BASE_DIR, "asset")
-else:
-    MEDIA_ROOT = "/home/kwekxbyu/kwekapi.com/asset"
+# STATIC_ROOT = "/home/kwekxbyu/kwekapi.com/static/"
+# if DEBUG:
+#     MEDIA_ROOT = os.path.join(BASE_DIR, "asset")
+# else:
+#     MEDIA_ROOT = "/home/kwekxbyu/kwekapi.com/asset"
 
-django_heroku.settings(locals())
+# ?/////////////////////////////Heroku
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+# ?/////////////////////////////Heroku
+
 
--- kwek/namecheap_settings.py
+++ kwek/namecheap_settings.py
@@ -1,29 +1,19 @@
 
-
 import os
+import django_heroku
 from .general_settings import *
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_URL = "/static/"
+MEDIA_URL = "/asset/"
 
-# STATIC_URL = "/static/"
-# MEDIA_URL = "/asset/"
-
-# STATICFILES_DIRS = [BASE_DIR, "static"]
+STATICFILES_DIRS = [BASE_DIR, "static"]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-# STATIC_ROOT = "/home/kwekxbyu/kwekapi.com/static/"
-# if DEBUG:
-#     MEDIA_ROOT = os.path.join(BASE_DIR, "asset")
-# else:
-#     MEDIA_ROOT = "/home/kwekxbyu/kwekapi.com/asset"
+STATIC_ROOT = "/home/kwekxbyu/kwekapi.com/static/"
+if DEBUG:
+    MEDIA_ROOT = os.path.join(BASE_DIR, "asset")
+else:
+    MEDIA_ROOT = "/home/kwekxbyu/kwekapi.com/asset"
 
-# ?/////////////////////////////Heroku
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-# ?/////////////////////////////Heroku
-
+django_heroku.settings(locals())
 
