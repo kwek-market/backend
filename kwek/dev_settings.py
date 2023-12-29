0699c3f0f2cc2e7baa756948f993c6ea59ee7e81
Nwokoye Chigozie <gregoflash05@gmail.com>
2023-12-29 11:50:31 +0100

    check

--- kwek/dev_settings.py
+++ kwek/dev_settings.py
@@ -15,9 +15,9 @@ from .general_settings import *
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 BACKEND_DOMAIN = "http://127.0.0.1:8000"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', "static")
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
