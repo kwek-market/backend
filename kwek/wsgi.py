9111d2b85150e8fc32b50386c380255f9052d646
Nwokoye Chigozie <gregoflash05@gmail.com>
2024-11-04 13:08:36 +0100

    test without jobs

--- kwek/wsgi.py
+++ kwek/wsgi.py
@@ -11,12 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from market.jobs import start_market_jobs_scheduler
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kwek.settings')
 
 application = get_wsgi_application()
 app = application
 
-
+# from market.jobs import start_market_jobs_scheduler
 # start_market_jobs_scheduler()
