633f58d60a686817a980e2273d9f7fe180ed06f2
Nwokoye Chigozie <gregoflash05@gmail.com>
2025-01-05 16:18:42 +0100

    set test env

--- kwek/test_settings.py
+++ kwek/test_settings.py
@@ -5,10 +5,10 @@ from .settings import *
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "test_database",  
-        "USER": "test_user",  
-        "PASSWORD": "test_password",  
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": os.getenv("TEST_DATABASE", "test_database"),  
+        "USER": os.getenv("TEST_USER", "test_user"),  
+        "PASSWORD": os.getenv("TEST_PASSWORD", "test_password"),  
+        "HOST": os.getenv("TEST_HOST", "localhost"),
+        "PORT": os.getenv("TEST_PORT", "5432"),
     }
 }
